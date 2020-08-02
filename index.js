c732013dd32bfde69b6e6a9bb031b4a1084f6722
villagef <filwyd123@gmail.com>
2020-08-02 12:36:34 +0200

    hamburger and navbar --> JS

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//map init
 function initMap() {
   let map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 50.04598, lng: 19.95088 },
@@ -90,3 +91,57 @@ function initMap() {
     draggable: false,
   });
 }
+
+//hamburger and navbar
+$(function () {
+  let headerElem = $("header");
+  let logo = $("#logo");
+  let navMenu = $("#nav-menu");
+  let navToggle = $("#nav-toggle");
+
+  navToggle.on("click", () => {
+    navMenu.css("right", "0");
+  });
+
+  $("#close-flyout").on("click", () => {
+    navMenu.css("right", "-100%");
+  });
+
+  $(document).on("click", (e) => {
+    let target = $(e.target);
+    if (
+      !(
+        target.closest("#nav-toggle").length > 0 ||
+        target.closest("#nav-menu").length > 0
+      )
+    ) {
+      navMenu.css("right", "-100%");
+    }
+  });
+
+  $(document).scroll(() => {
+    let scrollTop = $(document).scrollTop();
+
+    if (scrollTop > 0) {
+      navMenu.addClass("is-sticky");
+      logo.css("color", "#000");
+      headerElem.css("background", "#fff").css("opacity", "0.9");
+      navToggle.css("border-color", "#000");
+      navToggle.find(".strip").css("background-color", "#000");
+    } else {
+      navMenu.removeClass("is-sticky");
+      logo.css("color", "#fff");
+      headerElem.css("background", "transparent");
+      navToggle.css("bordre-color", "#fff");
+      navToggle.find(".strip").css("background-color", "#fff");
+    }
+
+    headerElem.css(
+      scrollTop >= 200
+        ? { padding: "0.5rem", "box-shadow": "0 -4px 10px 1px #999" }
+        : { padding: "1rem 0", "box-shadow": "none" }
+    );
+  });
+
+  $(document).trigger("scroll");
+});
